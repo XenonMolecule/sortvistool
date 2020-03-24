80eb81226b378af79003a8af239d55e2fe563e91
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-03-24 01:17:31 -0400

    Filled in table with content

--- src/components/sorttable.js
+++ src/components/sorttable.js
@@ -1,21 +1,65 @@
 import React from 'react';
 import Table from "react-bootstrap/Table"
+import {faAngleUp} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 class SortTable extends React.Component {
 
+  constructor(props) {
+    super(props);
+    let index = 10;
+    let history = [];
+    let swapHist = [];
+    history.push(props.sequence.slice(0,10));
+    while(index < props.sequence.length) {
+      swapHist.push(props.sequence.slice(index, index+2));
+      history.push(props.sequence.slice(index+2,index+12));
+      index+=12;
+    }
+    this.state = {
+      index: 0,
+      history: history,
+      swapHist: swapHist,
+    }
+  }
+
+  determinePointerCombo(i) {
+    let output = [];
+    if(this.state.history[this.state.index][i] === this.state.swapHist[this.state.index][0]) {
+      output.push(<FontAwesomeIcon icon={faAngleUp} style={{'color':'red'}}/>)
+    }
+    if(this.state.history[this.state.index][i] === this.state.swapHist[this.state.index][1]) {
+      output.push(<FontAwesomeIcon icon={faAngleUp} style={{'color':'blue'}}/>)
+    }
+    return output;
+  }
+
   render() {
+    const currSequence = this.state.history[this.state.index];
+    let headers = [];
     let rows = [];
-    for(let i = 0; i < 10; i ++) {
-      rows.push(<th key={i} style={{'textAlign':'center'}}>{i}</th>);
+    let pointers = [];
+    for(let i = 0; i < currSequence.length; i ++) {
+      headers.push(<th key={i} style={{'textAlign':'center'}}>{i}</th>);
+      rows.push(<td key={currSequence[i]} style={{'textAlign':'center'}}>{currSequence[i]}</td>);
+      pointers.push(<td key={i} style={{'textAlign':'center'}}>{this.determinePointerCombo(i)}</td>);
     }
     return (
       <>
         <Table striped bordered hover size="sm">
           <thead>
           <tr>
-            {rows}
+            {headers}
           </tr>
           </thead>
+          <tbody>
+          <tr>
+            {rows}
+          </tr>
+          <tr>
+            {pointers}
+          </tr>
+          </tbody>
         </Table>
       </>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import Layout from "../components/layout"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SortTable from "../components/sorttable"
 import { Container } from "react-bootstrap"
+import Row from "react-bootstrap/Row"
 
 function IndexPage(props) {
   const query = getJsonFromUrl(props.location.search)
   return (
     <Layout>
       <Container>
-        <SortTable sequence={query['s']} version={query['v']}/>
+        <Row>
+          <SortTable sequence={query['s']} version={query['v']}/>
+        </Row>
       </Container>
     </Layout>
   );
